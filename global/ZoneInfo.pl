7457e31c0691e26689ea0225691fc664deab8245
N0ctrnl <kurt@gilpin.info>
2015-10-11 10:37:28 -0500

    Seneschal Aldikar

--- global/ZoneInfo.pl
+++ global/ZoneInfo.pl
@@ -12,7 +12,7 @@ sub EVENT_SAY {
 
   if ($status >= 200){                       #must be gm status 200 or higher
     if($str=~/Hail/i){
-      plugin::DiaWind("Hello $name.  You are of GM status, level $status.  I will follow your [commands].");
+      quest::say("Hello $name.  You are of GM status, level $status.  I will follow your [commands].");
     }
     if($count==6){
       ($firstword, $secondword,$thirdword,$fourthword,$fifthword,$sixthword) = $str =~ m/(.*\ )(.*\ )(.*\ )(.*\ )(.*\ )(.*)$/;
@@ -20,18 +20,18 @@ sub EVENT_SAY {
       ($firstword, $secondword,$thirdword,$fourthword,$fifthword) = $str =~ m/(.*\ )(.*\ )(.*\ )(.*\ )(.*)$/;
     }elsif($count==4){
       ($firstword, $secondword,$thirdword,$fourthword) = $str =~ m/(.*\ )(.*\ )(.*\ )(.*)$/;
-#      plugin::DiaWind("First Word = $firstword and Second Word = $secondword and Third Word = $thirdword and Fourth Word = $fourthword");
+#      quest::say("First Word = $firstword and Second Word = $secondword and Third Word = $thirdword and Fourth Word = $fourthword");
     }elsif($count==3){
       ($firstword, $secondword,$thirdword) = $str =~ m/(.*\ )(.*\ )(.*)$/;
-#      plugin::DiaWind("First Word = $firstword and Second Word = $secondword and Third Word = $thirdword");
+#      quest::say("First Word = $firstword and Second Word = $secondword and Third Word = $thirdword");
     }elsif ($count==2){
       ($firstword, $secondword) = $str =~ m/(.*\ )(.*)$/;
-#      plugin::DiaWind("First Word = $firstword and Second Word = $secondword");
+#      quest::say("First Word = $firstword and Second Word = $secondword");
     }elsif($count==1){
       $firstword = $str;
-#      plugin::DiaWind("You only said $firstword");
+#      quest::say("You only said $firstword");
     }else{
-      plugin::DiaWind("Too many words");
+      quest::say("Too many words");
     }
 
 my $chr = chop($secondword);
@@ -46,110 +46,110 @@ my $chr = chop($sixthword);
 # Note: $count will always be one word higher than what was typed due to junk word added
 
     if($firstword=~/command/i){
-      plugin::DiaWind("I follow these commands:  [follow],[depop],[getcondition],[setcondition],[open],[close],[setglobal],[getglobal],[delglobal]");
+      quest::say("I follow these commands:  [follow],[depop],[getcondition],[setcondition],[open],[close],[setglobal],[getglobal],[delglobal]");
     }
 
     if($firstword=~/follow/i){
       if(($count==3) && ($secondword=~/on/i)){
-        plugin::DiaWind("I will follow $name");
+        quest::say("I will follow $name");
         quest::follow($userid);
       }elsif(($count==3) && ($secondword=~/off/i)){
-        plugin::DiaWind("I will stop following you.");
+        quest::say("I will stop following you.");
         quest::sfollow();
       }else{
-        plugin::DiaWind("Usage:  Follow <on|off>");
-        plugin::DiaWind("This command turns my follow mode on or off.");
+        quest::say("Usage:  Follow <on|off>");
+        quest::say("This command turns my follow mode on or off.");
       }
     }
     
     if($firstword=~/depop/i){
-      plugin::DiaWind("I will depop.");
+      quest::say("I will depop.");
       quest::depop();
     }
 
 
     if($firstword=~/getcondition/i){
       if($count==4){
-        plugin::DiaWind("You have requested I set get spawn condition $thirdword for zone $secondword.  I will do that now.");
+        quest::say("You have requested I set get spawn condition $thirdword for zone $secondword.  I will do that now.");
         my $spawnvalue = quest::get_spawn_condition("$secondword",$thirdword+=0);
-        plugin::DiaWind("It is currently set at $spawnvalue");
+        quest::say("It is currently set at $spawnvalue");
       }else{
-        plugin::DiaWind("Usage: getcondition <zonename> <id>");
-        plugin::DiaWind("This command tells you the value for the spawn condition <id> for the specified <zonename>.");
+        quest::say("Usage: getcondition <zonename> <id>");
+        quest::say("This command tells you the value for the spawn condition <id> for the specified <zonename>.");
       }
     }
 
     if($firstword=~/setcondition/i){
       if($count==5){
-        plugin::DiaWind("You have requested I set spawn condition $thirdword to $fourthword in zone $secondword.  I will do that now.");
+        quest::say("You have requested I set spawn condition $thirdword to $fourthword in zone $secondword.  I will do that now.");
         quest::spawn_condition("$secondword",$thirdword+=0,$fourthword+=0);
       }else{
-        plugin::DiaWind("Usage: setcondition <zonename> <id> <value>");
-        plugin::DiaWind("This command sets the spawn condition for <id> to <value> in the <zonename> you specify.");
+        quest::say("Usage: setcondition <zonename> <id> <value>");
+        quest::say("This command sets the spawn condition for <id> to <value> in the <zonename> you specify.");
       }
-      plugin::DiaWind("Please note:  The target zone MUST be reloaded before the spawn condition change will be recognized, UNLESS the condition you change is for the zone you are currently in.");
+      quest::say("Please note:  The target zone MUST be reloaded before the spawn condition change will be recognized, UNLESS the condition you change is for the zone you are currently in.");
     }
 
     if($firstword=~/open/i){
       if($count==3){
-        plugin::DiaWind("You have requested that I open door $secondword.  I will do so now.");
+        quest::say("You have requested that I open door $secondword.  I will do so now.");
         quest::forcedooropen($secondword+=0);
       }else{
-        plugin::DiaWind("Usage: Open <doorid>");
-        plugin::DiaWind("This command opens the door specified by <doorid>.  This only works with doors in the current zone.");
+        quest::say("Usage: Open <doorid>");
+        quest::say("This command opens the door specified by <doorid>.  This only works with doors in the current zone.");
       }
     }
     
     if($firstword=~/close/i){
       if($count==3){
-        plugin::DiaWind("You have requested that I close door $secondword.  I will do so now.");
+        quest::say("You have requested that I close door $secondword.  I will do so now.");
         quest::forcedoorclose($secondword+=0);
       }else{
-        plugin::DiaWind("Usage: Close <doorid>");
-        plugin::DiaWind("This command closes the door specified by <doorid>.  This only works with doors in the current zone.");
+        quest::say("Usage: Close <doorid>");
+        quest::say("This command closes the door specified by <doorid>.  This only works with doors in the current zone.");
       }
     }
     
     if($firstword=~/faction/i){
       if($count==4){
-        plugin::DiaWind("You have requested I change your faction number $secondword by $thirdword.  I will do that now.");
+        quest::say("You have requested I change your faction number $secondword by $thirdword.  I will do that now.");
         quest::faction($secondword+=0,$thirdword+=0);
       }else{
-        plugin::DiaWind("Usage: Faction <factionid> <value>");
-        plugin::DiaWind("This command changes your faction with <factionid> by the number of points specified in <value>.  Negative values will reduce faction");
+        quest::say("Usage: Faction <factionid> <value>");
+        quest::say("This command changes your faction with <factionid> by the number of points specified in <value>.  Negative values will reduce faction");
       }
     }
     
     if($firstword=~/setglobal/i){
       if($count==6){
-        plugin::DiaWind("You want me to set qglobal \$$secondword to value $thirdword, with the parameters $fourthword, $fifthword.  I will do that now.");
+        quest::say("You want me to set qglobal \$$secondword to value $thirdword, with the parameters $fourthword, $fifthword.  I will do that now.");
         quest::setglobal("$secondword",$thirdword,"$fourthword","$fifthword");
       }else{
-        plugin::DiaWind("Usage: setglobal <globalname> <value> <options> <duration>");
-        plugin::DiaWind("I will set global <globalname> to <value> with the specified <options> and <duration>.");
-        plugin::DiaWind("Please Note: Using incorrect <options> may result in changing the wrong global, or creating one no other npcs or zones may see.  Except under certain condiions, it is impractical to use this for options other than 3 or 7.");
+        quest::say("Usage: setglobal <globalname> <value> <options> <duration>");
+        quest::say("I will set global <globalname> to <value> with the specified <options> and <duration>.");
+        quest::say("Please Note: Using incorrect <options> may result in changing the wrong global, or creating one no other npcs or zones may see.  Except under certain condiions, it is impractical to use this for options other than 3 or 7.");
       }
     }
     
     if($firstword=~/getglobal/i){
       if($count==3){
-        plugin::DiaWind("Your qgolbal variable \$$secondword is currently set to $qglobals{$secondword}.");
+        quest::say("Your qgolbal variable \$$secondword is currently set to $qglobals{$secondword}.");
       }else{
-        plugin::DiaWind("Usage: getglobal <globalname>.");
-        plugin::DiaWind("This command tells you the value of <globalname>.");
-        plugin::DiaWind("Please Note:  The global's options must have been set correctly for this to work.  I cannot tell you the value of a global that I do not have access to.");
+        quest::say("Usage: getglobal <globalname>.");
+        quest::say("This command tells you the value of <globalname>.");
+        quest::say("Please Note:  The global's options must have been set correctly for this to work.  I cannot tell you the value of a global that I do not have access to.");
       }
     }
     
     if($firstword=~/delglobal/i){
       if($count==3){
-        plugin::DiaWind("Your qgolbal variable \$$secondword will be deleted.");
+        quest::say("Your qgolbal variable \$$secondword will be deleted.");
         quest::delglobal("$secondword");
         $qglobals{$secondword} = $undef;
       }else{
-        plugin::DiaWind("Usage:  delglobal <globalname>");
-        plugin::DiaWind("This command deletes the global <globalname>.");
-        plugin::DiaWind("Please Note:  I cannot delete globals that don't belong to me.");
+        quest::say("Usage:  delglobal <globalname>");
+        quest::say("This command deletes the global <globalname>.");
+        quest::say("Please Note:  I cannot delete globals that don't belong to me.");
       }
     }
     
--- thurgadinb/Seneschal_Aldikar.pl
+++ thurgadinb/Seneschal_Aldikar.pl
@@ -1,64 +1,84 @@
 #my $x = npc->GetX();
 #my $y = npc->GetY();
 #my $z = npc->GetZ();
-my $dainring
-my $spawnvalue
-#my $spawnvalue1 = quest::get_spawn_condition("thurgadinb",1);
-#my $spawnvalue2 = quest::get_spawn_condition("thurgadinb",2);
+my $dainring = undef;
 
 sub EVENT_SIGNAL {
 	if($signal==1) {
-		quest::moveto(5,780,38,130,1);
+#		quest::moveto(5,780,38,130,1);
+		quest::stop();
+		quest::start(31);
+		$sig1 = 1;
+		$sig2 = 0;
+  $dainring = 0;
 	}
 	if($signal==2) {
-		quest::moveto(-3,693,69,126,1);
+#		quest::moveto(-3,693,69,126,1);
+		quest::stop();
+		quest::start(30);
+		$sig1 = 0;
+		$sig2 = 1;
+  $dainring = 0;
 	}
 }
 
-#sub EVENT_SPAWN {
-#}
-
-sub EVENT_ENTER {
-#  quest::settimer("thurgdaynight",5);
-   $dainring = 0;
-   $spawnvalue = quest::get_spawn_condition("thurgadinb",1);
-#   $spawnvalue2 = quest::get_spawn_condition("thurgadinb",2);
+sub EVENT_SPAWN {
+  quest::stop();
+  if(quest::get_spawn_condition("thurgadinb",1) == 1) {
+  quest::start(32);
+  }
+  elsif (quest::get_spawn_condition("thurgadinb",2) == 1) {
+  quest::stop();
+  quest::start(30);
+  }
 }
 
 sub EVENT_SAY{
-  if($text=~/I will accept this task/i && $dainring==0){
+  if($text=~/I will accept this task/i && $dainring == 1){
     quest::say("In this box, place the accursed dirk of the fallen Rodrick. With it combine the heads of every traitor you dispose of. When this is done give the box and the velium insignia ring to the Dain directly. On behalf of the crown and all good Coldain, I thank you ... May Brell be with you.");
     quest::summonitem("17055"); 
-    quest::settimer("thurgdaynight",5);
-  }
-  else {
-    quest::say("No Dice!");
+#    quest::settimer("thurgdaynight",5);
   }
 }
 
 sub EVENT_ITEM {
-#  if(plugin::check_handin(\%itemcount, 30164 => 1) && $x == -3 && $y == 693 && $z == 68.5) {
-  if(plugin::check_handin(\%itemcount, 30164 => 1) && $spawnvalue == 1) {
+#  if(plugin::check_handin(\%itemcount, 30164 => 1) && $x == -3 && $y == 693 && $z == 69) {
+  if(plugin::check_handin(\%itemcount, 30164 => 1) && (quest::get_spawn_condition("thurgadinb",2) == 1)) {
     quest::say("Well done %t, I have heard of your victory over the Ry'Gorr. If you are willing to assist the crown further please follow me.");
     quest::summonitem(30164);
-    quest::stoptimer("thurgdaynight");
-    quest::settimer(10,1);
+    quest::stop();
+#    quest::signalwith(129000,1,0);
+    quest::moveto(-3.32909,694.677,69.7207);
+    quest::moveto(5.10136,687.487,68.6393);
+    quest::moveto(5.48169,656.502,63.7207);
+    quest::moveto(-37.5086,604.156,63.7207);
+    quest::moveto(-103.722,603.927,53.7207);
+    quest::moveto(-103.932,584.837,48.7207);
+    quest::moveto(-23.1887,585.836,38.752);
+    quest::moveto(-22.5928,712.132,38.752);
+    quest::moveto(4.53335,714.458,38.752);
+    quest::moveto(5,780,38);
+    quest::settimer(10,60);
+#    quest::resume();
+#    quest::start(31);
+#    quest::pause(30);
+#    quest::movepc(129, 5.30, 771.86, 38.73, 253);
+#    quest::stoptimer("thurgdaynight");
+#######    quest::settimer(10,10);
   }
-#  elsif(plugin::check_handin(\%itemcount, 30164 => 1) && $x == 5 && $y == 780 && $z == 38) {
-  elsif(plugin::check_handin(\%itemcount, 30164 => 1) && $spawnvalue == 0) {
-  #elsif(plugin::check_handin(\%itemcount, 30164 => 1) && $x == 5 && $y == 780 && $z == 38) {
+#  elsif(plugin::check_handin(\%itemcount, 30164 => 1) && $x == 3.25 && $y == 773.25 && $z == 35) {
+#  elsif(plugin::check_handin(\%itemcount, 30164 => 1) && (quest::get_spawn_condition("thurgadinb",1) == 1)) {
+  elsif(plugin::check_handin(\%itemcount, 30164 => 1) && (quest::get_spawn_condition("thurgadinb",1) == 1)) {
     quest::say("I must speak to the Dain before I instruct you further. Please speak to me while the royal court is in session.");
     quest::summonitem(30164);
-  }
-  else {
-    plugin::return_items(\%itemcount);
   }
 }
 
 sub EVENT_TIMER {
-  elsif($timer == 10 && $x == 5 && $y == 780 && $z == 38) {
+#  if($timer == 10 && $x == 5 && $y == 780 && $z == 38) {
+  if($timer == 10) {
     quest::stoptimer(10);
-    quest::pause(100);
+    quest::stop();
     quest::say("Please, shut the door behind you. What I am about to share with you must not be overheard.");
     quest::settimer(11,10);
   }
@@ -70,17 +90,28 @@ sub EVENT_TIMER {
   elsif($timer == 12) {
     quest::stoptimer(12);
     quest::say("It seems Rodrick was not alone in his treachery. There is a faction of Coldain who believe that a treaty should be signed with the Kromrif, ending our hostilities with them. This, of course, is impossible. If there is one thing our history here has taught us it is that the Kromrif simply cannot be trusted.");
-    quest::settimer(13);
+    quest::settimer(13,10);
   }
   elsif($timer == 13) {
     quest::stoptimer(13);
     quest::say("These traitors are poisoning the minds of our citizens, promising great rewards to those who will betry the Dain. It will take the unbiased eye of an outlander to flush out the masterminds behind this plan. Once again we turn to you.");
-    quest::settimer(14);
+    quest::settimer(14,10);
   }
   elsif($timer == 14) {
     quest::stoptimer(14);
     quest::say("Will you accept this task outlander?");
     $dainring = 1;
+    quest::settimer(15,60);
+  }
+  elsif($timer == 15) {
+    if(quest::get_spawn_condition("thurgadinb",2) == 1) {
+    quest::stoptimer(15);
+    quest::start(30);
+    }
+    elsif (quest::get_spawn_condition("thurgadinb",1) == 1) {
+    quest::stoptimer(15);
+    quest::start(31);
+    }
   }
 }
 
