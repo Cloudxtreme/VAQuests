71109f90e8f8b251f3415c0cca5b854ef5003665
N0ctrnl <kurt@gilpin.info>
2016-01-25 22:21:51 -0600

    PoM scripts

---
+++ nexus/port.pl
@@ -0,0 +1,42 @@
+sub EVENT_POPUPRESPONSE {
+  if ($popupid == 666) { #hateplaneb
+    quest::movepc(186,-393,656,3);
+  }
+  if ($popupid == 674) { #airplane
+    quest::movepc(71,539,1384,-664);
+  }
+  if ($popupid == 15891) { #dragonscale
+    #quest::movepc(442,-1954,3916,19);
+  }
+  if ($popupid == 2708) { #northkarana
+    quest::movepc(13,1209,-3685,-5);
+  }
+  if ($popupid == 4176) { #ecommons
+    quest::movepc(22,-140,-1520,3);
+  }
+  if ($popupid == 534) { #lavastorm
+    quest::movepc(27,460,460,-86);
+  }
+  if ($popupid == 2707) { #tox
+    quest::movepc(38,-916,-1510,-33);
+  }
+  if ($popupid == 2706) { #gfaydark
+    quest::movepc(54,-441,-2023,4);
+  }
+  if ($popupid == 2709) { #dreadlands
+    quest::movepc(86,9658,3047,1052);
+  }
+  if ($popupid == 2284) { #iceclad
+    quest::movepc(110,385,5321,-17);
+  }
+  if ($popupid == 2031) { #coboltscar
+    quest::movepc(117,-1634,-1065,299);
+  }
+  if ($popupid == 3615) { #twilight
+    quest::movepc(170,-1028,1338,39);
+  }
+  if ($popupid == 3794) { #stonebrunt
+    quest::movepc(100,673,-4531,0);
+  }
+:
+
