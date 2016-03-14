2b47eafe811ea3d8b4860ecaf98daacd26039b7d
N0ctrnl <kurt@gilpin.info>
2016-03-14 03:04:47 -0500

    Item scripts for illusion clickies

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -1,7 +1,7 @@
 sub EVENT_SPAWN {
   $roll = int(rand(1000)) + 1;
 
-  if($roll >= 995) {
+  if($roll >= 996 && $mlevel >= 46) {
     #my $itemid = ( int rand 95000 ) + 1001;
     my $itemid=(132520);
 
