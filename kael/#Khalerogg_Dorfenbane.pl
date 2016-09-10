d30c00b5d9321952f092623023974280232c86cb
N0ctrnl <kurt@gilpin.info>
2016-09-09 19:09:45 -0500

    Modifications to Doldigun event in Kael

--- kael/#Khalerogg_Dorfenbane.pl
+++ kael/#Khalerogg_Dorfenbane.pl
@@ -1,3 +1,7 @@
 sub EVENT_AGGRO {
-  quest::say("Your bones will be crushed by the Kromrif of Kael Drakkel!");
+  quest::say("Your bones will be crushed by the Kromrif of Kael Drakkel! Sir!");
+}
+
+sub EVENT_DEATH_COMPLETE {
+  quest::signalwith(113553,1);
 }
