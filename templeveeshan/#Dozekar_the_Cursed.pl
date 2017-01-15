d02f5ca3833aa6f3ffca33a1b93f6f0122b54ee9
N0ctrnl <kurt@gilpin.info>
2017-01-15 13:58:31 -0600

    Update Doze script to avoid a zone crash if #repop is issued when he's engaged.

--- templeveeshan/#Dozekar_the_Cursed.pl
+++ templeveeshan/#Dozekar_the_Cursed.pl
@@ -6,10 +6,16 @@ sub EVENT_SPAWN {
 }
 
 sub EVENT_TIMER {
+
   $LeashedID = $entity_list->GetMobByNpcTypeID(124037);
   if ($timer eq "CheckLeash") {
     quest::stoptimer("CheckLeash");
-    if($x > -1100 || $y > -950 || $y < -1200) {
+    if (!$LeashedID || $LeashedID->IsCorpse()) { #if the mob isn't up
+      
+      return;
+    }
+
+    if ($x > -1100 || $y > -950 || $y < -1200) {
       $LeashedID->GMMove($SpawnX, $SpawnY, $SpawnZ, $SpawnH);
     }
     quest::settimer("CheckLeash",1);
